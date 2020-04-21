e99b0b3d83498c2b57fbf2fb0f8cd5a7b1f29b99
Avuluri Venkatas Sai Reddy <avulurivenkatasaireddy@gmail.com>
2020-04-21 21:00:47 +0530

    commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-
+import pickle
 
 from preprocessor_and_predictor import get_data
 
@@ -10,6 +10,12 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('page1.html')
+
+
+    
+
+    
+    
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -22,11 +28,43 @@ def predict():
 
     answer=get_data(link)[0]
 
+
+
+@app.route('/automated_testing',methods=['POST'])
+def automate():
+
+    print("hi")
+    
+    content = request.files['file'].read().decode("utf-8")
+
+
+    content=open(content,"rb").readlines()
+
+    
+
+    
+    print(content)
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    links = [x.strip() for x in content]
+
+       
+
+    answers=[get_data(x.decode("utf-8"))[0] for x in links]
+
+    res = dict(zip(links, answers))
+
+    print(res)
+
+    resp = jsonify(res)
+
+
+    resp.status_code = 200
+    print(resp)
+    return resp
+
     
     
 
-    return render_template('page2.html', prediction_text=answer)
-
-
+    
 if __name__ == "__main__":
     app.run(debug=True)
