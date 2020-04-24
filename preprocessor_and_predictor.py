e913e113c9960fcd470b2e92028f16ff9213b6e8
Avuluri Venkatas Sai Reddy <avulurivenkatasaireddy@gmail.com>
2020-04-24 16:38:28 +0530

    commit

--- preprocessor_and_predictor.py
+++ preprocessor_and_predictor.py
@@ -20,11 +20,11 @@ from collections import Counter
 
 
 
-# from keras_preprocessing.text import tokenizer_from_json
+from keras_preprocessing.text import tokenizer_from_json
 
 import joblib
-#import tensorflow as tf
-#from tensorflow.keras.preprocessing.sequence import pad_sequences
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 from wordcloud import STOPWORDS
 import praw
@@ -99,47 +99,47 @@ def get_data(links):
     
 
     model1=joblib.load("pipeline1.pkl")
-    #model2=joblib.load("pipeline.pkl")
-    #model3=tf.keras.models.load_model("lstm1.h5")
+    model2=joblib.load("pipeline.pkl")
+    model3=tf.keras.models.load_model("lstm1.h5")
 
-    # with open('tokenizer.json') as f:
-    # 	data = json.load(f)
-    # 	tokenizer = tokenizer_from_json(data)
+    with open('tokenizer.json') as f:
+    	data = json.load(f)
+    	tokenizer = tokenizer_from_json(data)
 
-    # lb1=joblib.load("label_biarizer.pkl")
+    lb1=joblib.load("label_biarizer.pkl")
 
     # answer=[]
     arr1=model1.predict(df.combined)
-#     arr2=model2.predict(df.combined)
+    arr2=model2.predict(df.combined)
 
-#     max_length=100
+    max_length=100
 
-#     trunc_type='post'
-#     padding_type='post'
-#     tokens=tokenizer.texts_to_sequences(df.combined)
+    trunc_type='post'
+    padding_type='post'
+    tokens=tokenizer.texts_to_sequences(df.combined)
 
-#     padded=pad_sequences(tokens, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+    padded=pad_sequences(tokens, maxlen=max_length, padding=padding_type, truncating=trunc_type)
                                         
-#     arr3=lb1.inverse_transform(model3.predict(padded))
+    arr3=lb1.inverse_transform(model3.predict(padded))
 
 
-# #     print(answer)
+#     print(answer)
     
     
-#     m = np.vstack([arr1,arr2,arr3])
+    m = np.vstack([arr1,arr2,arr3])
     
     
     
     
     
-#     modes_counter = []
+    modes_counter = []
 
-#     for c in m.T:
-#         modes_counter.append(Counter(c).most_common(1)[0][0])
+    for c in m.T:
+        modes_counter.append(Counter(c).most_common(1)[0][0])
         
         
     
-    res = dict(zip(links,list(arr1)))
+    res = dict(zip(links,modes_counter))
 
 
 #     resp = jsonify(res)
